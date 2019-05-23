c213ffe7173644b42b7f02fdeb966d1324b47ab1
Bill Hicks <bhicks-lookback@darkstarstudios.com>
2019-05-23 07:21:21 -0500

    Update FroopTests.kt

--- app/src/test/java/io/lookback/froopkotlin/FroopTests.kt
+++ app/src/test/java/io/lookback/froopkotlin/FroopTests.kt
@@ -1,8 +1,7 @@
 package io.lookback.froopkotlin
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 import java.util.concurrent.Semaphore
 import kotlin.concurrent.thread
 
